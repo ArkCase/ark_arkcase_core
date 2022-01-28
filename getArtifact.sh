a7bc66b643d871c19313ecd1969d17ed5c11977d
naveen-armedia <Naveen.Kerati@armedia.com>
2022-01-28 15:58:35 +0530

    updated with args

--- getArtifact.sh
+++ getArtifact.sh
@@ -6,6 +6,5 @@ ARKCASE_VERSION="2021.03-RC11"
 rm -rf artifacts
 mkdir artifacts
 echo "Downloading Arkcase core artifacts $ARKCASE_VERSION"
-export ARKCASE_VERSION
 aws s3 cp "s3://arkcase-container-artifacts/arkcase-core/${ARKCASE_VERSION}/artifacts/" artifacts/ --recursive
 
